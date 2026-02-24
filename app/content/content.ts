de169590000011c49bedc208546f17376053349e
Kenneth Yu <mail@kennethyu.dev>
2026-02-23 21:35:14 -0500

    content change

--- app/content/content.ts
+++ app/content/content.ts
@@ -118,11 +118,11 @@ export const contactUs = {
     heading: "Contact \n Us",
     phoneNumber: { text: "718-972-6620 (Shop)", number: 7189726620 },
     cellNumber: { text: "917-295-1205 (Cell)", number: 9172951205 },
-    email: "theTDAuto@gmail.com",
+    email: "thetdauto@gmail.com",
   },
   rightSection: {
     heading: "Business \n Hours",
-    hours: "Mon - Fri: 8:00 - 5:30 \n Sat: 8:00 - 5:00 \n Sun: CLOSED",
+    hours: "Mon - Fri: 8:00 - 5:00 \n Sat: 8:00 - 5:00 \n Sun: CLOSED",
     address: "896 4th Avenue, Brooklyn NY 11232",
   },
 } as const;
